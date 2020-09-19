52f7ead6517167e4043e8d11369bd751ac6f69cf
Jorge Cervantes <jorgecdev@gmail.com>
2020-09-19 01:03:30 -0500

    se separo las rutas de auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesRoutingModule } from './pages/pages-routing';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from "./auth/register/register.component";
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 const routes: Routes = [  
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent },
 ];
 
@@ -19,7 +15,8 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
