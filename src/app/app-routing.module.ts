cd88063f67a4171bf35173c13e09b56cead580fb
Jorge Cervantes <jorgecdev@gmail.com>
2020-09-19 00:44:17 -0500

    Se separo en modulos shared, pages, auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from "./auth/register/register.component";
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
 
 
