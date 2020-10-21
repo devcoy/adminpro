3348ca07d764ad597df00146e156afd30992174b
Jorge Cervantes <jorgecdev@gmail.com>
2020-10-21 14:42:22 -0500

    servicio de menu

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    {
+      title: 'Dashboard',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        {
+          title: 'Inicio',
+          url: '/',
+        },
+        {
+          title: 'ProgressBar',
+          url: 'progress',
+        },
+        {
+          title: 'Gráfica',
+          url: 'graphics1',
+        }
+      ]
+    }
+  ]
+  constructor() { }
+}
