37fd5811866fe23da1ac9f7d94def5e881fa57f7
Jorge Cervantes <jorge.cervantes@beyondtechnology.net>
2020-10-21 13:03:42 -0500

    servicios de account settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -10,17 +11,16 @@ export class AccountSettingsComponent implements OnInit {
   
   linkTheme = document.querySelector('#theme');
 
-  constructor() { }
+  constructor(
+    private settingsService: SettingsService
+  ) { }
 
   ngOnInit(): void {
     
   }
 
   changeTheme(theme: string) {
-    const url = `./assets/css/colors/${ theme }.css`;
-
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
+    this.settingsService.changeTheme( theme );
   }
 
 }
