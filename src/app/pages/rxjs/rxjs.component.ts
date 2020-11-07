a2b608dde07f42d82d88c9b1efdcb6a70d353d27
Jorge Cervantes <jorgecdev@gmail.com>
2020-11-06 21:47:05 -0600

    manejo de observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    const obs$ = new Observable( observer => {
+
+      let i = -1;
+      const intervalo = setInterval( () => {
+
+        i++;
+        observer.next(i);
+        if ( i === 4 ) {
+
+          clearInterval( intervalo );
+          observer.complete();
+        }
+
+        if (i === 2) {
+
+          observer.error('i llego al valor de 2');
+
+        }
+
+      }, 1000);
+    });
+
+    obs$.subscribe( value => {
+      console.log('[Subs] ', value);
+    },
+    err => console.warn('[Error] ', err),
+    () => console.log('[Terminado] Obs terminado'));
+  }
 
   ngOnInit(): void {
   }
