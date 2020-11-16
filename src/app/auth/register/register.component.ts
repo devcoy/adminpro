69a2a100923a8e4026ab8ff264cb34b2f13a35a1
Jorge Cervantes <jorgecdev@gmail.com>
2020-11-15 23:04:36 -0600

    validación de nombre, email y terminos

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,13 +8,13 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
-
-    registerForm = this.fb.group({
+    public formSubmitted = false;
+    public registerForm = this.fb.group({
 
         name: ['Jorge Cervantes', [Validators.required, Validators.minLength(3)]],
-        email: ['dummy1@email.com', [Validators.required, Validators.email]],
-        password: ['qwerty123', [Validators.required, Validators.minLength(8)]],
-        password2: ['qwerty123', [Validators.required, Validators.minLength(8)]],
+        email: ['jorge@email.com', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        password2: ['', [Validators.required, Validators.minLength(8)]],
         terms: [false, [Validators.required]],
     });
 
@@ -26,7 +26,35 @@ export class RegisterComponent {
 
 
     createUser() {
+
+        this.formSubmitted = true;
         console.log(this.registerForm.value);
+
+        if( this.registerForm.valid ) {
+
+            console.warn('Formulario correcto');
+
+        } else {
+
+            console.warn('Formulario incorrecto');
+        }
+    }
+
+
+    fieldNotValid( field: string ): boolean {
+
+        if( this.registerForm.get( field ).invalid && this.formSubmitted ) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+
+    acceptTerms() {
+        return !this.registerForm.get('terms').value && this.formSubmitted;
     }
 
 
