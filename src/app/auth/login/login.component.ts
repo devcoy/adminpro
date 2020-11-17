7623aabe101a2a1e044f89227437f799a27c4761
Jorge Cervantes <jorgecdev@gmail.com>
2020-11-16 18:44:01 -0600

    obtener el token de google sign in

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Validators, FormBuilder } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 
 import Swal from 'sweetalert2';
+declare const gapi: any;
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
     public formSubmitted = false;
 
     public loginForm = this.fb.group({
 
-        email: [ localStorage.getItem('email') || '', [Validators.required, Validators.email]],
-        password: [ '', [Validators.required, Validators.minLength(8)]],
-        remember: [ false ]
+        email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        remember: [false]
 
     });
 
@@ -31,13 +32,18 @@ export class LoginComponent {
         private userService: UserService
     ) { }
 
+
+    ngOnInit(): void {
+        this.renderButton();
+    }
+
     login(): void {
 
-        this.userService.login( this.loginForm.value )
-            .subscribe( resp => {
+        this.userService.login(this.loginForm.value)
+            .subscribe(resp => {
 
-                if( this.loginForm.get('remember').value) {
-                    localStorage.setItem('email', this.loginForm.get('email').value );
+                if (this.loginForm.get('remember').value) {
+                    localStorage.setItem('email', this.loginForm.get('email').value);
                 } else {
                     localStorage.removeItem('email');
                 }
@@ -48,6 +54,28 @@ export class LoginComponent {
                 Swal.fire('Error', err.error.msg, 'error');
             });
 
+    }
+
+
+    onSuccess(googleUser) {
+        // console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+        console.log(googleUser.getAuthResponse().id_token);
+    }
+    onFailure(error) {
+        console.log(error);
+    }
+
+    renderButton() {
+        gapi.signin2.render('my-signin2', {
+            'scope': 'profile email',
+            'width': 240,
+            'height': 50,
+            'display': 'flex',
+            'longtitle': true,
+            'theme': 'dark',
+            'onsuccess': this.onSuccess,
+            'onfailure': this.onFailure
+        });
     }
 
 
