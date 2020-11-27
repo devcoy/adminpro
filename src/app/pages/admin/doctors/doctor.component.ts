e5eed118efc6bee57755e788688119fc05101567
Jorge Cervantes <jorgecdev@gmail.com>
2020-11-26 21:03:49 -0600

    Listar los hospitales en el select

--- src/app/pages/admin/doctors/doctor.component.ts
+++ src/app/pages/admin/doctors/doctor.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { HospitalService } from '../../../services/hospital.service';
+import { Hospital } from '../../../models/hospitals.model';
 
 
 @Component({
@@ -9,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorComponent implements OnInit {
 
-  constructor() { }
+  doctorForm: FormGroup;
+  hospitals: Hospital[];
+
+  constructor(
+    private fb: FormBuilder,
+    private hospitalService: HospitalService
+  ) { }
 
   ngOnInit(): void {
+
+    this.doctorForm = this.fb.group({
+      nombre: [ 'Jorge', Validators.required],
+      hospital: [ '' ]
+    });
+
+
+    this.loadHospitals();
+
+  }
+
+
+
+
+  loadHospitals() {
+
+    this.hospitalService.loadHospitals().subscribe( ( hospitals: Hospital[] ) => {
+
+      console.log(hospitals);
+      this.hospitals = hospitals;
+
+
+    }, error => {
+      console.log('[Error al intentar cargar los hospitales]', error);
+    });
+
+  }
+
+
+
+
+  saveDoctor() {
+
+    console.log(this.doctorForm.value);
+
   }
 
 
